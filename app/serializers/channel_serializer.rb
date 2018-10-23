a0c26f9ec13e514056d553af118787d68207049c
henek <eagluhih@gmail.com>
2018-10-23 19:45:16 +0500

    Добавлен slug

--- app/serializers/channel_serializer.rb
+++ app/serializers/channel_serializer.rb
@@ -2,7 +2,7 @@
 
 class ChannelSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :description, :youtube_id, :youtube_url, :avatar, :created_at
+  attributes :id, :title, :description, :youtube_id, :youtube_url, :avatar, :created_at, :slug
   attribute :videos_count do |record|
     record.videos.to_a.count
   end
